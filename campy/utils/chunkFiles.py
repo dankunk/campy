4606d20e65adb458cc1f42f1c97276a293aa5bc8
Daniel Kunk <daniel.kunk@colostate.edu>
2024-12-13 14:18:54 -0700

    Updating chunkFiles for Camera0 directory and chunklengthinsec instead of in frames

--- campy/utils/chunkFiles.py
+++ campy/utils/chunkFiles.py
@@ -1,3 +1,4 @@
+
 import os
 import imageio
 import math
@@ -7,74 +8,77 @@ import time
 import multiprocessing as mp
 
 numCams = int(sys.argv[1])
-chunkLengthInFrames = int(sys.argv[2])
+chunkLengthInSec = float(sys.argv[2])  # User provides seconds directly
 fname = str(sys.argv[3])
 
 basedir = os.getcwd()
 
 # Get metadata from video (assuming all cameras are the same)
-vid1 = os.path.join(basedir,'Camera1',fname)
+vid1 = os.path.join(basedir,'Camera0',fname)
 vid = imageio.get_reader(vid1)
 fps = vid.get_meta_data()['fps']
 durationInSec = vid.get_meta_data()['duration']
-durationInFrames = int(fps*durationInSec)
-chunkLengthInSec = chunkLengthInFrames/fps
-numChunks = math.ceil(durationInFrames/chunkLengthInFrames)
+durationInFrames = int(fps * durationInSec)
+
+# Compute chunk length in frames from chunk length in seconds
+chunkLengthInFrames = int(fps * chunkLengthInSec)
+
+# Compute how many chunks are needed
+numChunks = math.ceil(durationInFrames / chunkLengthInFrames)
 
 def chunkFiles(camNum):
     startFrame = 0
     endFrame = startFrame + chunkLengthInFrames - 1
     startTimeInSec = 0
-    timeInSec = 0
     hrsStart = 0; minStart = 0; secStart = 0; msStart = 0
 
-    viddir = os.path.join(basedir,'Camera' + str(camNum+1))
-    fname_in = os.path.join(viddir,fname)
-    for t in range(0,numChunks):
+    viddir = os.path.join(basedir,'Camera' + str(camNum))
+    fname_in = os.path.join(viddir, fname)
 
+    for t in range(numChunks):
         outdir = os.path.join(viddir,'workspace')
         if not os.path.isdir(outdir):
             os.mkdir(outdir)
-        fname_out = os.path.join(outdir,str(startFrame) + '_' + str(endFrame) + '.mp4')
-        
-        # No need to pad zeros
-        startTime = str(hrsStart) + ':' + str(minStart) + ':' + str(secStart) + '.' + str(msStart)
+        fname_out = os.path.join(outdir,f"{startFrame}_{endFrame}.mp4")
 
+        # Calculate startTime from hrsStart, minStart, secStart, msStart
+        startTime = f"{hrsStart}:{minStart}:{secStart}.{msStart}"
+
+        # Compute endTime from startTimeInSec + chunkLengthInSec
         timeEnd = startTimeInSec + chunkLengthInSec
         hr = math.floor(timeEnd/3600)
-        timeEnd = timeEnd - hr*3600
+        timeEnd -= hr*3600
         mn = math.floor(timeEnd/60)
-        timeEnd = timeEnd - mn*60
+        timeEnd -= mn*60
         sc = math.floor(timeEnd)
-        timeEnd = timeEnd - sc
+        timeEnd -= sc
         ms = math.floor(timeEnd*1000)
 
-        endTime = str(hr) + ':' + str(mn) + ':' + str(sc) + '.' + str(ms)
+        endTime = f"{hr}:{mn}:{sc}.{ms}"
 
-        cmd = ('ffmpeg -y -i ' + fname_in + ' -ss ' + startTime + ' -to ' + endTime + 
-        ' -c:v copy -c:a copy ' + fname_out + ' -async 1 '
-        ' -hide_banner -loglevel warning')
+        cmd = (f"ffmpeg -y -i {fname_in} -ss {startTime} -to {endTime} "
+               f"-c:v copy -c:a copy {fname_out} -async 1 "
+               "-hide_banner -loglevel warning")
 
         if os.path.isfile(fname_out):
-            print('Video ' + str(camNum+1) + ' chunk ' + str(t) + ' already exists...')
+            print(f"Video {camNum+1} chunk {t} already exists...")
         else:
             p = Popen(cmd.split())
-            print('Copying video ' + str(camNum+1) + ' chunk ' + str(t) + '...')
+            print(f"Copying video {camNum+1} chunk {t}...")
             time.sleep(5)
 
-        startFrame = startFrame + chunkLengthInFrames
+        startFrame += chunkLengthInFrames
         endFrame = startFrame + chunkLengthInFrames - 1
         if endFrame > durationInFrames:
             endFrame = durationInFrames
-        startTimeInSec = startTimeInSec + chunkLengthInSec
+        startTimeInSec += chunkLengthInSec
         hrsStart = hr
         minStart = mn
         secStart = sc
         msStart = ms
 
 if __name__ == '__main__':            
-    
     ts = time.time()
     print('Chunking videos...')
     pp = mp.Pool(numCams)
-    pp.map(chunkFiles,range(0,numCams))
+    pp.map(chunkFiles, range(numCams))
